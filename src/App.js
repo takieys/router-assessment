dd8b9b6361c266833f5ef289a81cb2b840be0e65
Takieya Stanley <takieyaas823@gmail.com>
2021-01-29 16:45:27 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+
+import "./App.css";
+
+import Welcome from "./Components/Welcome/Welcome";
+import Clock from "./Components/Clock/Clock";
+import Contact from "./Components/Contact/Contact";
+import Navigation from "./Components/navigation/Navigation";
+import notFound from "./Components/notFound/notFound";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navigation />
+      <Switch>
+        <Route
+          exact
+          path="/"
+          render={(props) => <Welcome {...props} name="Takieya" />}
+        />
+        <Route path="/welcome/:name" component={person} />
+
+        <Route path="/clock" component={Clock} />
+        <Route path="/contact" component={Contact} />
+        <Route component={notFound} />
+      </Switch>
     </div>
   );
 }
+
+const person = ({ match }) => <div>Welcome, {match.params.name} !</div>;
 
 export default App;
---
+++ src/Components/notFound/notFound.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function notFound(props) {
+  return <div className="notFound">Not Found</div>;
+}
+
+export default notFound;
